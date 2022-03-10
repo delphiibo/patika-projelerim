63310b79b88a2e5387c9256584b211f1f683d961
delphiibo <delphiibo@hotmail.com>
2022-03-11 01:53:40 +0300

    odev2

---
+++ odev2/Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_2
+{
+    class Program
+    {
+        public static void WriteStarLine(string str)
+        {
+            Console.WriteLine("***** " + str + " *****");
+        }
+
+        static int InsertNumber(int index)
+        {
+            Console.Write("Lütfen {0}. sayıyı giriniz : ", index);
+            int number = 0;
+            bool isNumeric = int.TryParse(Console.ReadLine(), out number);
+            if (isNumeric == false)
+            {
+                Console.WriteLine("{0}. değeri hatalı girdiniz!", index);
+                InsertNumber(index);
+                return 0;
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("{0}. değeri negatif değer olamaz!", index);
+                InsertNumber(index);
+                return 0;
+            }
+            else
+                return number;
+        }
+
+        static void Main(string[] args)
+        {
+            ArrayList Numbers = new ArrayList();
+            ArrayList MaxNumbers = new ArrayList();
+            ArrayList MinNumbers = new ArrayList();
+
+            for (var i = 1; i <= 20; i++)
+            {
+                int number = InsertNumber(i);
+                Numbers.Add(number);
+            }
+
+            Numbers.Sort();
+            MinNumbers.Add(Numbers[0]);
+            MinNumbers.Add(Numbers[1]);
+            MinNumbers.Add(Numbers[2]);
+            Numbers.Reverse();
+            MaxNumbers.Add(Numbers[0]);
+            MaxNumbers.Add(Numbers[1]);
+            MaxNumbers.Add(Numbers[2]);
+
+            WriteStarLine("Girdiniz Sayılardan En Büyük 3 Sayı");
+            MaxNumbers.Print();
+            WriteStarLine("Girdiniz Sayılardan En Küçük 3 Sayı");
+            MinNumbers.Print();
+            double MaxNumbersAvarege = MaxNumbers.Average();
+            double MinNumbersAvarage = MinNumbers.Average();
+            double AverageTotal = MaxNumbersAvarege+MinNumbersAvarage;
+            Console.WriteLine("En Büyük 3 Sayının Ortalaması = {0}",MaxNumbersAvarege);
+            Console.WriteLine("En Küçük 3 Sayının Ortalaması = {0}",MinNumbersAvarage);
+            Console.WriteLine("En Büyük 3 Sayının Ortalaması ve En Küçük 3 Sayının Ortalaması Toplamı = {0}",AverageTotal);
+        }
+    }
+    public static class Extension {
+        public static void Print(this ArrayList arrList)
+        {
+            foreach (var item in arrList)
+                Console.WriteLine(item);
+        }
+
+        public static double Average(this ArrayList arrList)
+        {
+            if (arrList.Count<=0)
+                return 0;
+            double result = 0;
+            foreach (var item in arrList)
+                result += Convert.ToDouble(item);
+            return result/arrList.Count;
+        }
+        
+    }
+}
---
+++ odev2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static Dictionary<string,int> LetterCalc(ArrayList arrList)
+        {
+            Dictionary<string,int> list = new Dictionary<string,int>();
+            foreach (var item in arrList)
+            {
+                if (list.ContainsKey(item.ToString())==false)
+                {
+                    int count =0;
+                    for (var i=0;i<arrList.Count;i++)
+                    {
+                        if (Convert.ToString(arrList[i])==item.ToString())
+                            count++;
+                    }
+                    list.Add(item.ToString(),count);
+                }
+            }
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            ArrayList vowelList = new ArrayList();
+            char[] vowels = {'a','e','ı','i','o','ö','ü','A','E','I','İ','O','Ö','Ü'};
+            Console.Write("Lütfen bir cümle giriniz : ");
+            string sentence = Console.ReadLine();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (Array.IndexOf(vowels,sentence[i])>-1)
+                    vowelList.Add(sentence[i].ToString().ToLower());
+            }
+            Console.WriteLine("Girdiğiniz Cümle İçerisinde Toplam {0} Sesli Harf Var.",vowelList.Count);
+            vowelList.Sort();
+            Console.WriteLine("Sesli harlerin listesi : ");
+            Dictionary<string,int> liste = LetterCalc(vowelList);
+            foreach (var item in liste)
+                Console.WriteLine("{0} harfi {1} kere tekrar ediyor",item.Key,item.Value);
+        }
+    }
+}
